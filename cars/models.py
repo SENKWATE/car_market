6e09defd234feb0b76d7d728099a4a2e50368862
Khalid Mohamed <kalmohammed56@gmail.com>
2018-10-10 14:54:50 +0300

    draft

--- cars/models.py
+++ cars/models.py
@@ -5,9 +5,21 @@ class Car(models.Model):
 	make = models.CharField(max_length=120)
 	model = models.CharField(max_length=120)
 	year = models.IntegerField()
+	car_img=models.ImageField(null=True, blank=True)
 
 	def __str__(self):
 		return "{} {} - {}".format(self.make, self.model, self.year)
 
 	def get_absolute_url(self):
 		return reverse('car-detail', kwargs={'car_id':self.id})
+
+# 	def get_image_filename(instance, filename):
+#     title = instance.post.title
+#     slug = slugify(title)
+#     return "post_images/%s-%s" % (slug, filename)  
+
+
+# class Images(models.Model):
+#     post = models.ForeignKey(Post, default=None)
+#     image = models.ImageField(upload_to=get_image_filename,
+#                               verbose_name='Image)
--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Car
+from .forms import CarForm
+from django.contrib import messages
 
 def car_list(request):
 	cars = Car.objects.all()
@@ -19,14 +21,37 @@ def car_detail(request, car_id):
 
 def car_create(request):
 	#Complete Me
-	return render(...)
+	form = CarForm()
+	if request.method == "POST":
+		form = CarForm(request.POST, request.FILES or None)
+		if form.is_valid():
+			form.save()
+			messages.add_message(request, messages.INFO, 'Car Added')
+			return redirect('car-list')
+	context = {
+		"form":form,
+	}
+	return render(request, 'create.html', context)
+
 
 
 def car_update(request, car_id):
-	#Complete Me
-	return render(...)
+	car_obj = Car.objects.get(id=car_id)
+	form = CarForm(instance=car_obj)
+	if request.method == "POST":
+		form = CarForm(request.POST,request.FILES or None , instance=car_obj)
+		if form.is_valid():
+			form.save()
+			messages.add_message(request, messages.INFO, 'Successfully Updated.')
+			return redirect('car-list')
+	context = {
+		"car_obj": car_obj,
+		"form":form,
+	}
+	return render(request, 'update.html', context)
 
 
 def car_delete(request, car_id):
-	#Complete Me
-	return render(...)
+	car_obj = Car.objects.get(id=car_id)
+	car_obj.delete()
+	return redirect('car-list')
